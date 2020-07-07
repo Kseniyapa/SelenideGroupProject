ecc1d23b6f3946c49948bd1f52373508d272a65d
ioann <ioanntrapeznikov@gmail.com>
2020-07-07 18:28:10 +0300

    Added test for auth client.

--- src/test/java/com/pflb/pages/tests/AuthorizationTest.java
+++ src/test/java/com/pflb/pages/tests/AuthorizationTest.java
@@ -1,4 +1,20 @@
 package com.pflb.pages.tests;
 
+import com.pflb.pages.AuthorizationPage;
+import org.testng.annotations.Test;
+
+
 public class AuthorizationTest {
+    private static final String URL = "https://test.uxcrowd.ru/";
+    private final String email = "wibddmtssk@1secmail.com";
+    private final String password = "PqxPZR";
+
+    @Test
+    public void authClient() {
+        AuthorizationPage authorizationPage = AuthorizationPage.openBrowser(URL);
+        authorizationPage.authClient(email, password);
+
+    }
+
+
 }
