3b700042a222c870efd8aa604214caf145ed6ae4
kseniyapanshina <kseniyapanshinana@gmail.com>
2020-07-07 22:04:34 +0300

    CreateNewTest

--- src/main/java/com/pflb/pages/CreateNewTestPage.java
+++ src/main/java/com/pflb/pages/CreateNewTestPage.java
@@ -1,4 +1,60 @@
 package com.pflb.pages;
 
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+import static com.codeborne.selenide.Condition.visible;
+
 public class CreateNewTestPage {
+    @FindBy(how = How.XPATH, using = "//*[className = \"new_test\"]")
+    private SelenideElement buttonCreateNewTest; //  Кнопка "Новый тест".
+
+    @FindBy(how = How.XPATH, using = "//*[css = \"[data-testid=\"Test name input\"]]")
+    private SelenideElement fieldTestName; // Поле "Название теста".
+
+    @FindBy(how = How.XPATH, using = "//*[css = \"[data-testid=\"Address site input\"]")
+    private SelenideElement fieldWebSite; // Поле "Адрес сайта".
+
+    @FindBy(how = How.XPATH, using = "//*[css = \"[data-testid=\"Information textarea\"]")
+    private SelenideElement fieldInformationForRespondent; // Поле "Вводная информация для респондента".
+
+    @FindBy(how = How.XPATH, using = "//*[css = \"[data-testid=\"Submit button\"]")
+    private SelenideElement buttonSelectAudience; // Кнопка "К выбору аудитории".
+
+    @FindBy(how = How.XPATH, using = "//*[css = \"[data-testid=\"Segment name input 0\"]")
+    private SelenideElement fieldNameSegment; // Поле "Название сегмента".
+
+    @FindBy(how = How.XPATH, using = "//*[css = \"[data-testid=\"Tasks button\"]")
+    private SelenideElement buttonToTasks; // Кнопка "К заданиям".
+
+    @FindBy(how = How.XPATH, using = "//*[css = \"[data-testid=\"Tasks button\"]")
+    private SelenideElement fieldVoiceResponse; // Поле "Голосовой ответ".
+
+    @FindBy(how = How.XPATH, using = "//*[css = \"[data-testid=\"Tasks submit task\"]")
+    private SelenideElement buttonAdded; // Кнопка "Добавить".
+
+    @FindBy(how = How.XPATH, using = "//*[css = \"[data-testid=\"Check button\"]")
+    private SelenideElement buttonCheckAndRun; // Кнопка "Проверка и запуск".
+
+    @FindBy(how = How.XPATH, using = "//*[css = \"[data-testid=\"Checkout start button\"]")
+    private SelenideElement buttonRunFreeTest;// Кнопка "Запустиьть бесплатный тест".
+
+    public void createNewTest() {
+        buttonCreateNewTest();
+        fieldTestName.setValue("test");
+        fieldWebSite.setValue("test.com");
+        buttonSelectAudience.shouldBe(visible).pressEnter();
+        fieldNameSegment.setValue("test segment");
+        buttonToTasks.shouldBe(visible).pressEnter();
+        fieldVoiceResponse.setValue("some");
+        buttonAdded.shouldBe(visible).pressEnter();
+        buttonCheckAndRun.shouldBe(visible).pressEnter();
+        buttonRunFreeTest.shouldBe(visible).pressEnter();
+    }
+
+    private void buttonCreateNewTest() {
+        buttonCreateNewTest.shouldBe(visible).click();
+    }
+
 }
--- src/test/java/com/pflb/pages/tests/positivetests/CreateNewTest.java
+++ src/test/java/com/pflb/pages/tests/positivetests/CreateNewTest.java
@@ -1,4 +1,25 @@
 package com.pflb.pages.tests.positivetests;
 
-public class CreateNewTest {
+import com.pflb.pages.AuthorizationPage;
+import com.pflb.pages.CreateNewTestPage;
+import com.pflb.pages.RegistrationPage;
+import com.pflb.pages.tests.configuration.SettingsForTests;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Selenide.page;
+
+public class CreateNewTest extends SettingsForTests {
+    @BeforeClass
+    public void auth(){}
+
+    @Test
+    public void createNewTest() {
+        CreateNewTestPage page = page(CreateNewTestPage.class);
+        page.createNewTest();
+    }
+
 }
+
