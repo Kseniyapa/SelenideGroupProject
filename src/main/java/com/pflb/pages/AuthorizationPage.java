4782923a49601e0cbb80ee916abf144367dd41a1
ioann <ioanntrapeznikov@gmail.com>
2020-07-07 18:28:00 +0300

    Added class for auth client.

--- src/main/java/com/pflb/pages/AuthorizationPage.java
+++ src/main/java/com/pflb/pages/AuthorizationPage.java
@@ -1,4 +1,39 @@
 package com.pflb.pages;
 
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.open;
+
 public class AuthorizationPage {
+
+
+    @FindBy(how = How.XPATH, using = "//*[@id=\"header-lk-button\"]")
+    private SelenideElement openWindowAuth;
+    @FindBy(how = How.XPATH, using = "//*[@id=\"login\"]")
+    private SelenideElement fieldEmail;
+    @FindBy(how = How.XPATH, using = "//*[@id=\"form_auth\"]/input[2]")
+    private SelenideElement fieldPassword;
+    @FindBy(how = How.XPATH, using = "//*[@id=\"form_auth\"]/button")
+    private SelenideElement buttonEnter;
+
+
+    public void authClient(String email, String password) {
+        openFormAuth();
+        fieldEmail.setValue(email);
+        fieldPassword.setValue(password);
+        buttonEnter.shouldBe(visible).pressEnter();
+    }
+
+    private void openFormAuth() {
+        openWindowAuth.shouldBe(visible).click();
+    }
+
+    public static AuthorizationPage openBrowser(String url) {
+        return open(url, AuthorizationPage.class);
+    }
+
+
 }
