257d43fae369703ed1e45305be7de6b810b76739
ioann <ioanntrapeznikov@gmail.com>
2020-07-07 20:34:25 +0300

    Added test profile

--- src/test/java/com/pflb/pages/tests/positivetests/ProfilePageTest.java
+++ src/test/java/com/pflb/pages/tests/positivetests/ProfilePageTest.java
@@ -1,4 +1,39 @@
 package com.pflb.pages.tests.positivetests;
 
-public class ProfilePageTest {
+
+import com.pflb.pages.AuthorizationPage;
+import com.pflb.pages.ProfilePage;
+import com.pflb.pages.tests.configuration.SettingsForTests;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Selenide.page;
+
+public class ProfilePageTest extends SettingsForTests {
+
+    private static final String EXCEPTEDLOGIN = "testtempmail685";
+    private static final String EXCEPTEDEMAIL = "testtempmail685@1secmail.org";
+    private static final String PASSWORD = "NhBugH";
+    private static final String EXCEPTEDFIO = "Ivan685";
+
+
+    @BeforeTest
+    public void authClient() {
+        AuthorizationPage page = page(AuthorizationPage.class);
+        page.authClient(EXCEPTEDEMAIL, PASSWORD);
+    }
+
+    @Test(priority = 1)
+    public void checkValidEmail() {
+        ProfilePage page = page(ProfilePage.class);
+        page.clickProfileSettings();
+        String actualTextFromFiledNameClient = page.getTextFromFiledNameClient();
+        String actualTextFromFieldEmailClient = page.getTextFromFieldEmailClient();
+        String actualTextFromFiledClientFIO = page.getTextFromFiledClientFIO();
+        Assert.assertEquals(actualTextFromFiledNameClient, EXCEPTEDLOGIN);
+        Assert.assertEquals(actualTextFromFieldEmailClient, EXCEPTEDEMAIL);
+        Assert.assertEquals(actualTextFromFiledClientFIO, EXCEPTEDFIO);
+    }
+
 }
