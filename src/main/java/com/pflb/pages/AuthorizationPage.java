281abd9143e4c2e2829ed6db82e9478f299717ec
ioann <ioanntrapeznikov@gmail.com>
2020-07-07 20:33:43 +0300

    Change

--- src/main/java/com/pflb/pages/AuthorizationPage.java
+++ src/main/java/com/pflb/pages/AuthorizationPage.java
@@ -8,29 +8,56 @@ import static com.codeborne.selenide.Condition.visible;
 
 public class AuthorizationPage {
 
+    /**
+     * Локаторы для работы со страницей
+     */
 
     @FindBy(how = How.XPATH, using = "//*[@id=\"header-lk-button\"]")
-    private SelenideElement openWindowAuth;
+    private SelenideElement openWindowAuth; // // Кнопка открытия окна "Вход/Регистрация".
+
     @FindBy(how = How.XPATH, using = "//*[@id=\"login\"]")
-    private SelenideElement fieldEmail;
+    private SelenideElement fieldEmail; // Поле для ввода почты клиента.
+
     @FindBy(how = How.XPATH, using = "//*[@id=\"form_auth\"]/input[2]")
-    private SelenideElement fieldPassword;
+    private SelenideElement fieldPassword; // Поле для ввода пароля клиента.
+
     @FindBy(how = How.XPATH, using = "//*[@id=\"form_auth\"]/button")
-    private SelenideElement enterButton;
+    private SelenideElement enterButton; // Кнопка "Войти" в профиль клиента.
+
     @FindBy(how = How.XPATH, using = "//*[@id=\"logout\"]")
-    private SelenideElement logoutButton;
+    private SelenideElement logoutButton; // Кнопка "Выйти" в профиль клиента.
+
+
+    /**
+     * Метод проверяет отображается ли кнопка на странице профиля.
+     */
 
     public boolean isLogoutButton() {
         return logoutButton.isDisplayed();
     }
+
+    /**
+     * Метод авторизирует клианта , есть ли кнопка на странице профиля.
+     */
 
     public void authClient(String email, String password) {
         openFormAuth();
         fieldEmail.setValue(email);
         fieldPassword.setValue(password);
         enterButton.shouldBe(visible).pressEnter();
+    }
+
+    /**
+     * Метод нажимает кнопку "Выйти".
+     */
+
+    public void logoutClient() {
         logoutButton.shouldBe(visible).click();
     }
+
+    /**
+     * Метод открывает форму для авторизации.
+     */
 
     private void openFormAuth() {
         openWindowAuth.shouldBe(visible).click();
