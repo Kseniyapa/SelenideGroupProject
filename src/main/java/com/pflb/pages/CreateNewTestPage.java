12d9a2b42bb00c08a47f38fa9ff2b107c9d183c3
ioann <ioanntrapeznikov@gmail.com>
2020-07-07 23:57:52 +0300

    update

--- src/main/java/com/pflb/pages/CreateNewTestPage.java
+++ src/main/java/com/pflb/pages/CreateNewTestPage.java
@@ -1,60 +1,69 @@
 package com.pflb.pages;
 
 import com.codeborne.selenide.SelenideElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.How;
+import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
 
 public class CreateNewTestPage {
-    @FindBy(how = How.XPATH, using = "[ng-tr=\"MTP.MTP\"]")
-    private SelenideElement buttonCreateNewTest; //  Кнопка "Новый тест".
 
-    @FindBy(how = How.XPATH, using = "//*[css = \"[data-testid=\"Test name input\"]]")
-    private SelenideElement fieldTestName; // Поле "Название теста".
+    /**
+     * Локаторы для работы со страницей
+     */
 
-    @FindBy(how = How.XPATH, using = "//*[css = \"[data-testid=\"Address site input\"]")
-    private SelenideElement fieldWebSite; // Поле "Адрес сайта".
+    private SelenideElement createNewTestButton = $(By.className("new_test"));  // Открыть окно создания нового теста
 
-    @FindBy(how = How.XPATH, using = "//*[css = \"[data-testid=\"Information textarea\"]")
-    private SelenideElement fieldInformationForRespondent; // Поле "Вводная информация для респондента".
+    private SelenideElement fieldNameTest = $(By.cssSelector("[data-testid=\"Test name input\"]")); // Поле для ввода названия теста.
 
-    @FindBy(how = How.XPATH, using = "//*[css = \"[data-testid=\"Submit button\"]")
-    private SelenideElement buttonSelectAudience; // Кнопка "К выбору аудитории".
+    private SelenideElement fieldWebSite = $(By.cssSelector("[data-testid=\"Address site input\"]")); // Поле для ввода веб-сайта.
 
-    @FindBy(how = How.XPATH, using = "//*[css = \"[data-testid=\"Segment name input 0\"]")
-    private SelenideElement fieldNameSegment; // Поле "Название сегмента".
+    private SelenideElement goToAudience = $(By.cssSelector("[data-testid=\"Submit button\"]")); // Кнопка перехода к выбору аудитории.
 
-    @FindBy(how = How.XPATH, using = "//*[css = \"[data-testid=\"Tasks button\"]")
-    private SelenideElement buttonToTasks; // Кнопка "К заданиям".
+    private SelenideElement fieldNameSegment = $(By.cssSelector("[data-testid=\"Segment name input 0\"]")); // Поле для ввода названия сегмента.
 
-    @FindBy(how = How.XPATH, using = "//*[css = \"[data-testid=\"Tasks button\"]")
-    private SelenideElement fieldVoiceResponse; // Поле "Голосовой ответ".
+    private SelenideElement goToQuestButton = $(By.cssSelector("[data-testid=\"Tasks button\"]")); // Кнопка перехода к заданиям.
 
-    @FindBy(how = How.XPATH, using = "//*[css = \"[data-testid=\"Tasks submit task\"]")
-    private SelenideElement buttonAdded; // Кнопка "Добавить".
+    private SelenideElement fieldForQuestion = $(By.cssSelector("[data-testid=\"Tasks task question\"]")); // Поле для ввода вопроса.
 
-    @FindBy(how = How.XPATH, using = "//*[css = \"[data-testid=\"Check button\"]")
-    private SelenideElement buttonCheckAndRun; // Кнопка "Проверка и запуск".
+    private SelenideElement addedQuestionButton = $(By.cssSelector("[data-testid=\"Tasks submit task\"]")); // Кнопка добавления задания.
 
-    @FindBy(how = How.XPATH, using = "//*[css = \"[data-testid=\"Checkout start button\"]")
-    private SelenideElement buttonRunFreeTest;// Кнопка "Запустиьть бесплатный тест".
+    private SelenideElement checkAndRunTestButton = $(By.cssSelector("[data-testid=\"Check button\"]")); // Кнопка перехода к проверке и запуску теста.
 
-    public void createNewTest() {
-        buttonCreateNewTest();
-        fieldTestName.setValue("test");
-        fieldWebSite.setValue("test.com");
-        buttonSelectAudience.shouldBe(visible).pressEnter();
-        fieldNameSegment.setValue("test segment");
-        buttonToTasks.shouldBe(visible).pressEnter();
-        fieldVoiceResponse.setValue("some");
-        buttonAdded.shouldBe(visible).pressEnter();
-        buttonCheckAndRun.shouldBe(visible).pressEnter();
-        buttonRunFreeTest.shouldBe(visible).pressEnter();
+    private SelenideElement rubFreeTestButton = $(By.cssSelector("[data-testid=\"Checkout start button\"]")); // Запуск бесплатного теста.
+
+    private SelenideElement textAfterCreateNewTest = $(By.cssSelector("[class = \"sc-itybZL gLzIYc\"")); // Текст , который появляется после успешного создания теста.
+
+    /**
+     * Метод который возвращает текст.
+     *
+     * @return текст , который выводится после успешного создания теста от клиента.
+     */
+
+    public String getTextAfterCreateNewTest() {
+        return textAfterCreateNewTest.getText();
     }
 
-    private void buttonCreateNewTest() {
-        buttonCreateNewTest.shouldBe(visible).click();
+    /**
+     * Метод для создания нового теста от клиента.
+     *
+     * @param nameTest    Имя теста.
+     * @param website     Адрес сайта , который нужны протестировать.
+     * @param nameSegment Имя сегмента.
+     * @param question    Создать вопрос.
+     */
+
+    public void createNewTest(String nameTest, String website, String nameSegment, String question) {
+        createNewTestButton.shouldBe(visible).click();
+        fieldNameTest.setValue(nameTest);
+        fieldWebSite.setValue(website);
+        goToAudience.shouldBe(visible).pressEnter();
+        fieldNameSegment.setValue(nameSegment);
+        goToQuestButton.shouldBe(visible).pressEnter();
+        fieldForQuestion.setValue(question);
+        addedQuestionButton.shouldBe(visible).pressEnter();
+        checkAndRunTestButton.shouldBe(visible).pressEnter();
+        rubFreeTestButton.shouldBe(visible).pressEnter();
     }
 
 }
