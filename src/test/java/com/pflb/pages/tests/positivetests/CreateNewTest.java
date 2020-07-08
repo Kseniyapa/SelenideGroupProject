9b7cbee0499e13da1714b0599698aa86ea7e3e82
ioann <ioanntrapeznikov@gmail.com>
2020-07-08 18:11:37 +0300

    Refactor

--- src/test/java/com/pflb/pages/tests/positivetests/CreateNewTest.java
+++ src/test/java/com/pflb/pages/tests/positivetests/CreateNewTest.java
@@ -2,27 +2,49 @@ package com.pflb.pages.tests.positivetests;
 
 import com.pflb.pages.AuthorizationPage;
 import com.pflb.pages.CreateNewTestPage;
+import com.pflb.pages.RegistrationPage;
 import com.pflb.pages.tests.configuration.SettingsForTests;
-import io.qameta.allure.Step;
+import dataemail.TempMail;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.util.Random;
+
 import static com.codeborne.selenide.Selenide.page;
 
 public class CreateNewTest extends SettingsForTests {
+    /**
+     * Создание нового теста с подготовленными данными.
+     */
     private static final String EXCEPTEDTEXTAFTERCREATENEWTEST = "Ура! Тест создан и отправлен пользователям.";
-    private static final String EXCEPTEDEMAIL = "7yvadvrwwf@1secmail.com";
-    private static final String PASSWORD = "00Y8dI";
     private static final String NAMETEST = "AVANGARD";
     private static final String WEBSITE = "test.uxcrowd.ru";
     private static final String NAMESEGMENT = "SEGMENT";
     private static final String QUESTION = "added new question";
+    private final int randomNumber = new Random().nextInt(1000);
+    /**
+     * Регистрация с заранее подготовленными данными
+     */
+    private final String name = "Ivan" + randomNumber;
+    private final String position = "Junoir";
+    private final String company = "Perfomance";
+    private final String login = "testforregistration" + randomNumber;
+    private final String endMail = "@1secmail.com";
+    private final String phone = "79999999999";
+
 
     @BeforeClass
-    public void authClient() {
-        AuthorizationPage page = page(AuthorizationPage.class);
-        page.authClient(EXCEPTEDEMAIL, PASSWORD);
+    public void registrationNewClient() {
+        RegistrationPage registrationPage = page(RegistrationPage.class);
+        registrationPage.registrationNewClient(name, position, company, login + endMail, phone);
+
+        TempMail tempmail = page(TempMail.class);
+        String idMessage = tempmail.checkMail(login);
+        String passwordFromMessage = tempmail.getPasswordFromMessage(login, idMessage);
+
+        AuthorizationPage authorizationPage = page(AuthorizationPage.class);
+        authorizationPage.authClient(login + endMail, passwordFromMessage);
     }
 
     @Test
