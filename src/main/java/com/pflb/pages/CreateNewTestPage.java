fc88cb55ffa7d8aa7243bdc49668a38c747d1751
kseniyapanshina <kseniyapanshinana@gmail.com>
2020-07-09 12:04:05 +0300

    Message

--- src/main/java/com/pflb/pages/CreateNewTestPage.java
+++ src/main/java/com/pflb/pages/CreateNewTestPage.java
@@ -1,7 +1,6 @@
 package com.pflb.pages;
 
 import com.codeborne.selenide.SelenideElement;
-import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.visible;
